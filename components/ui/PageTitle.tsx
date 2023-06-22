2bc1340290a970a734af0eb13e8d74aeba7fdfee
kirkwood616 <kirkwood616@gmail.com>
2023-06-22 17:42:40 -0400

    added PageTitle component

---
+++ components/ui/PageTitle.tsx
@@ -0,0 +1,15 @@
+import styles from "styles/ui/PageTitle.module.css";
+
+interface PageTitleProps {
+  children: string;
+}
+
+export default function PageTitle({ children }: PageTitleProps) {
+  return (
+    <div className={styles.PageTitle}>
+      <div className={styles.bar}></div>
+      <h1>{children}</h1>
+      <div className={styles.bar}></div>
+    </div>
+  );
+}
