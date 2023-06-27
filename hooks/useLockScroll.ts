fb214274a836b01d584fe06251698d4b68ade751
kirkwood616 <kirkwood616@gmail.com>
2023-06-27 19:23:36 -0400

    corrected body shift

--- hooks/useLockScroll.ts
+++ hooks/useLockScroll.ts
@@ -2,15 +2,30 @@ import { useEffect } from "react";
 
 export default function useLockScroll(isActive: boolean) {
   useEffect(() => {
+    if (!isActive) {
+      return;
+    }
+
     const html = document.getElementsByTagName("html")[0];
+    const header = document.getElementsByTagName("header")[0];
+
+    const windowWidth = window.innerWidth;
+    const documentWidth = document.documentElement.clientWidth;
+    const scrollBarWidth = windowWidth - documentWidth;
 
     if (isActive) {
+      html.style.paddingRight = `${scrollBarWidth}px`;
+      header.style.paddingRight = `${scrollBarWidth}px`;
       html.classList.add("lock_scroll");
     } else {
+      html.style.paddingRight = "0px";
+      header.style.paddingRight = "0px";
       html.classList.remove("lock_scroll");
     }
 
     return () => {
+      html.style.paddingRight = "0px";
+      header.style.paddingRight = "0px";
       html.classList.remove("lock_scroll");
     };
   }, [isActive]);
