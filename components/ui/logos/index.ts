dcd3aba7ecbb8c89d714fa3a73a4ddd35c52f892
kirkwood616 <kirkwood616@gmail.com>
2023-07-11 11:18:19 -0400

    added logo components

---
+++ components/ui/logos/index.ts
@@ -0,0 +1,3 @@
+export { default as SonrisaFullOutlined } from "./SonrisaFullOutlined";
+export { default as SonrisaTeeth } from "./SonrisaTeeth";
+export { default as SonrisaTeethOutlined } from "./SonrisaTeethOutlined";
