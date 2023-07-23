1f6256e6d3fbcbb7dc3815c03f7ddb95e2ce54c7
kirkwood616 <kirkwood616@gmail.com>
2023-07-23 17:04:02 -0400

    changed to Teeth

--- components/ui/logos/index.ts
+++ components/ui/logos/index.ts
@@ -1,3 +1,3 @@
 export { default as SonrisaFullOutlined } from "./SonrisaFullOutlined";
-export { default as SonrisaTeeth } from "./SonrisaTeeth";
 export { default as SonrisaTeethOutlined } from "./SonrisaTeethOutlined";
+export { default as SonrisaTeeth } from "./Teeth";
