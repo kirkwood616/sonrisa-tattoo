6efe4d82cd543a8bd6842dfd0809c6405ea99306
kirkwood616 <kirkwood616@gmail.com>
2023-07-23 18:47:49 -0400

    updated styling

--- components/ui/header/MenuList.tsx
+++ components/ui/header/MenuList.tsx
@@ -13,19 +13,19 @@ export default function MenuList({ isNavActive }: MenuListProps) {
     <>
       <menu className={isNavActive ? styles.MenuList__active : styles.MenuList}>
         <Link href={"/"}>
-          <li className={router.pathname === "/" ? styles.active_page : ""}>HOME</li>
+          <li className={router.pathname === "/" ? styles.active_page : styles.menu_item}>HOME</li>
         </Link>
         <Link href={"/about"}>
-          <li className={router.pathname === "/about" ? styles.active_page : ""}>ABOUT</li>
+          <li className={router.pathname === "/about" ? styles.active_page : styles.menu_item}>ABOUT</li>
         </Link>
         <Link href={"/work"}>
-          <li className={router.pathname === "/work" ? styles.active_page : ""}>WORK</li>
+          <li className={router.pathname === "/work" ? styles.active_page : styles.menu_item}>WORK</li>
         </Link>
         <Link href={"/aftercare"}>
-          <li className={router.pathname === "/aftercare" ? styles.active_page : ""}>AFTERCARE</li>
+          <li className={router.pathname === "/aftercare" ? styles.active_page : styles.menu_item}>AFTERCARE</li>
         </Link>
         <Link href={"/contact"}>
-          <li className={router.pathname === "/contact" ? styles.active_page : ""}>CONTACT</li>
+          <li className={router.pathname === "/contact" ? styles.active_page : styles.menu_item}>CONTACT</li>
         </Link>
       </menu>
       <div className={isNavActive ? styles.underlay__active : styles.underlay}></div>
