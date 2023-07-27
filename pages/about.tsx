081595467db28aa760237e8110d2f0f8bbbc661e
kirkwood616 <kirkwood616@gmail.com>
2023-07-27 15:12:55 -0400

    styling changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -17,26 +17,28 @@ export default function About() {
       <div className={styles.About}>
         <PageBanner title={"About"} description={"All about us at Sonrisa Tattoo."} />
         <section className={styles.info__section}>
-          <div className={styles.image__container}>
-            <div className={styles.image__wrapper}>
-              <Image src={storefront} fill alt="Sonrisa Tattoo Shop storefront" />
+          <div className={styles.info__container}>
+            <div className={styles.image__container}>
+              <div className={styles.image__wrapper}>
+                <Image src={storefront} fill alt="Sonrisa Tattoo Shop storefront" />
+              </div>
             </div>
+            <article>
+              <InfoDiv>
+                <InfoHeading title={"Sonrisa Tattoo"} />
+                <p>
+                  Sonrisa Tattoo was founded in 2023 by owner Reuben Garcia. Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci nulla pellentesque
+                  dignissim enim sit amet venenatis.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Orci nulla pellentesque dignissim enim sit amet venenatis. Commodo odio aenean sed
+                  adipiscing diam donec.
+                </p>
+              </InfoDiv>
+            </article>
           </div>
-          <article>
-            <InfoDiv>
-              <InfoHeading title={"Sonrisa Tattoo"} />
-              <p>
-                Sonrisa Tattoo was founded in 2023 by owner Reuben Garcia. Lorem ipsum dolor sit amet, consectetur adipiscing
-                elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci nulla pellentesque dignissim
-                enim sit amet venenatis.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua. Orci nulla pellentesque dignissim enim sit amet venenatis. Commodo odio aenean sed adipiscing
-                diam donec.
-              </p>
-            </InfoDiv>
-          </article>
         </section>
       </div>
     </>
