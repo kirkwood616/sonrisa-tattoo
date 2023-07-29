d7929817fc943e4d0c9588ca96448c3446765956
kirkwood616 <kirkwood616@gmail.com>
2023-07-29 12:44:15 -0400

    added Icon components

---
+++ components/ui/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Bandages } from "./Bandages";
+export { default as Topicals } from "./Topicals";
+export { default as Warnings } from "./Warnings";
+export { default as Washing } from "./Washing";
